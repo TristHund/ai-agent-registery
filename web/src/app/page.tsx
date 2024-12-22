0ba6bd58af519762c9a16b3d6f0c658425a7418d
Aarsho <chatterjeeaarsho@gmail.com>
2024-12-22 08:52:51 +0530

    wallet button

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,32 +1,16 @@
 import * as React from "react";
 
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import RegisterCard from "@/components/default/registerCard";
 import { AccordionForm } from "@/components/default/accordionForm";
+import PhantomWalletButton from "@/components/default/phantomButton";
 
 export default function Home() {
   return (
-    <div className="bg-zinc-950 dark:bg-white text-center h-screen p-20 flex items-center content-center flex-col">
+    <div className="bg-zinc-950 dark:bg-white text-center min-h-[100vh] p-20 flex items-center content-center flex-col">
       <h1 className="scroll-m-20 text-4xl text-white font-extrabold tracking-tight lg:text-5xl">
         CRCT - Agent Registry
       </h1>
+      <PhantomWalletButton />
       <h2 className="scroll-m-20 text-white pb-2 text-3xl font-semibold tracking-tight mt-12">
         Search for agents registered
       </h2>
